f38a3d65c32cce7ae9b73a784490d5126cc89456
Bhuvanesh87 <bhuvanesh.arveti@gmail.com>
2018-10-26 21:10:33 +0530

    first commit

---
+++ Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.Models;
+
+namespace Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MvcEmployeeClass> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<MvcEmployeeClass>>().Result;
+            return View(empList);
+        }
+    }
+} 
